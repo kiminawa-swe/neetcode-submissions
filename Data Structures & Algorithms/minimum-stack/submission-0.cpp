cc5af920630465bf0dee4e65caacf04871d5b064
kiminawa-swe <kimi.suhardi07@gmail.com>
2026-07-04 11:46:12 +0800

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+
+private:
+stack<int>st;
+stack<int>min_st;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+
+        if(min_st.empty()){
+            min_st.push(val);
+        }
+        else{
+            min_st.push(min(val,min_st.top()));
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        min_st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+        
+    }
+    
+    int getMin() {
+        return min_st.top();
+        
+    }
+};
