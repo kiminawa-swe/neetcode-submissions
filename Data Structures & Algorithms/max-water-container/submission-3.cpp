cd0701f73c063d0db17f7a5256c8027468598f3c
kiminawa-swe <kimi.suhardi07@gmail.com>
2026-07-07 11:10:01 +0800

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        
+        int left=0;
+        int right=heights.size()-1;
+        int maxArea=0;
+
+        while(left<right){
+
+            int area=(right-left)*(min(heights[left],heights[right]));
+            maxArea=max(maxArea,area);
+
+            if(heights[left]<heights[right]){
+                left++;
+            }
+            else{
+                //if heights[right]<heigts[left]
+                right--;
+            }
+
+        }
+return maxArea;
+
+
+    }
+};
